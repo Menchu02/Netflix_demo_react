8dd8e53493e8b1955555063701c18b2bedb7b9e4
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-11-28 19:21:12 +0100

    añado botones Courses y Movies en Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,8 @@ export default function Navbar() {
   return (
     <div className={styles.navbarContainer}>
       <h1 className={styles.titleNetflix}>NetflixDemo</h1>
+      <button className={styles.navBarButton}>Courses</button>
+      <button className={styles.navBarButton}>Movies</button>
       <Link to={'/movieForm'}>
         <button className={styles.navBarButton}>Add movie</button>
       </Link>
