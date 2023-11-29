d5b20fefa267e51d0b8869dbdb2b586f465ebd38
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-11-29 11:37:57 +0100

    añado icono login, botones link de Courses y Movies en Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './navbar.module.css';
 import { Link } from 'react-router-dom';
+import { FaUserAlt } from 'react-icons/fa';
 
 export default function Navbar() {
   return (
     <div className={styles.navbarContainer}>
-      <h1 className={styles.titleNetflix}>NetflixDemo</h1>
-      <button className={styles.navBarButton}>Courses</button>
-      <button className={styles.navBarButton}>Movies</button>
-      <Link to={'/movieForm'}>
+      <div>
+        <h1 className={styles.titleNetflix}>NetflixDemo</h1>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.navBarButton}>Courses</button>
+        <button className={styles.navBarButton}>Movies</button>
+        <FaUserAlt className={styles.loginButton} />
+      </div>
+
+      {/* <Link to={'/movieForm'}>
         <button className={styles.navBarButton}>Add movie</button>
-      </Link>
+      </Link> */}
     </div>
   );
 }
