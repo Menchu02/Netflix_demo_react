0cf462e89f8586ba66471c0611b54c116ce5b951
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-11-29 12:38:31 +0100

    añado nuevo logo al Navbar.jsx y modifico color coorporativo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './navbar.module.css';
 import { Link } from 'react-router-dom';
-import { FaUserAlt } from 'react-icons/fa';
+import { FaCircleUser } from 'react-icons/fa6';
+import logo from '../../img/dilamica_rectangular.png';
 
 export default function Navbar() {
   return (
     <div className={styles.navbarContainer}>
       <div>
-        <h1 className={styles.titleNetflix}>NetflixDemo</h1>
+        {/* <h1 className={styles.titleNetflix}>NetflixDemo</h1> */}
+        <img src={logo} alt='photoLogo' className={styles.imgLogo} />
       </div>
       <div className={styles.buttonsContainer}>
-        <button className={styles.navBarButton}>Courses</button>
-        <button className={styles.navBarButton}>Movies</button>
-        <FaUserAlt className={styles.loginButton} />
+        <div className={styles.buttonContainer}>
+          <button className={styles.navBarButton}>Courses</button>
+          <button className={styles.navBarButton}>Movies</button>
+        </div>
+        <div>
+          <FaCircleUser className={styles.loginButton} />
+        </div>
       </div>
 
       {/* <Link to={'/movieForm'}>
