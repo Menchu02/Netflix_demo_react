99b4a5f772db480a5cc7c51b7bcc7d44250da4be
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-11-29 19:22:59 +0100

    añado botón Add new course en CoursesList.jsx

--- src/pages/MovieDetails/CoursList/CoursList.jsx
+++ src/pages/MovieDetails/CoursList/CoursList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import coursesService from '../../../api_service/courses_service';
 import CourseCard from '../CourseCard/CourseCard';
 import styles from './CourseList.module.css';
+import { Link } from 'react-router-dom';
 
 export default function CoursList() {
   const [courses, setCourses] = useState([]);
@@ -11,10 +12,15 @@ export default function CoursList() {
   }, []);
 
   return (
-    <div className={styles.coursesListContainer}>
-      {courses.map((item) => (
-        <CourseCard key={item.id} item={item} />
-      ))}
+    <div>
+      <Link to={'/movieForm'}>
+        <button className={styles.addCourseButton}>Add new course</button>
+      </Link>
+      <div className={styles.coursesListContainer}>
+        {courses.map((item) => (
+          <CourseCard key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
